891cd92ce43bd663c7d4658bf932bb82a8ecf844
Mitchell <Hmitchell.hall@gmail.com>
2022-01-14 11:46:01 -0600

    commit tree cd60ada102c43964c737912baeea1f455f8e80ef

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +18,25 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_record_not_found
+    render json: {error: 'Plant not found'}
   end
 end
